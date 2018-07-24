2b068d9de5b962ee9fe3831654a21aa107be428b
Inna Tarasyan <innatarasyanmail@gmail.com>
2018-07-24 18:43:14 +0400

    Code Enhancement

--- resources/views/theme/base.blade.php
+++ resources/views/theme/base.blade.php
@@ -4,6 +4,10 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <!-- Optional theme -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+        <meta name="description" content="I am Inna Tarasyan, I am web developer. ">
+        <meta name="keywords" content="Inna Tarasyan, Laravel, Freelancer, PHP">
+        <meta name="author" content="Inna Tarasyan">
+
         @yield('css')
     </head>
 <body>
