9506507aed23e5369bc6451fae411c0046035690
Inna Tarasyan <innatarasyanmail@gmail.com>
2024-02-18 20:06:04 +0400

    fixes

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,7 @@
 
 namespace App\Exceptions;
 
-use Exception;
+use Throwable;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -29,10 +29,12 @@ class Handler extends ExceptionHandler
     /**
      * Report or log an exception.
      *
+     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+     *
      * @param  \Exception  $exception
      * @return void
      */
-    public function report(Exception $exception)
+    public function report(Throwable $exception)
     {
         parent::report($exception);
     }
@@ -44,8 +46,9 @@ class Handler extends ExceptionHandler
      * @param  \Exception  $exception
      * @return \Illuminate\Http\Response
      */
-    public function render($request, Exception $exception)
+    public function render($request, Throwable $exception)
     {
         return parent::render($request, $exception);
     }
+
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use DB;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
@@ -25,7 +25,7 @@ class UsersTableSeeder extends Seeder
                 ],
                 [
                     'name' => 'Alex Tarasyan',
-                    'email' => 'atarasyan@gmail.com',
+                    'email' => 'atarasyan2@gmail.com',
                     'password' => '$2y$10$eL7EBf1C.w2HlEzcghGMYO/q2.Jb02RPBzS7QcCSbmE6T5bBdafKy',
                     'remember_token' => '8N6ikNE1o806YKGvRf6ILDqykxgrMV2h40SSg1YUnvuypIwK85RaFpKoTQKS',
                     'created_at' => '2018-06-17 21:00:00',
@@ -34,7 +34,7 @@ class UsersTableSeeder extends Seeder
                 ],
                 [
                     'name' => 'Irina Sagatelyan',
-                    'email' => 'atarasyan@gmail.com',
+                    'email' => 'atarasyan3@gmail.com',
                     'password' => '$2y$10$J1K5AYeTVVyLGiLjBC2bE.FtI4K/4AtFI2EruZfiUGsAAoo6KnhMq',
                     'remember_token' => '8N6ikNE1o806YKGvRf6ILDqykxgrMV2h40SSg1YUnvuypIwK85RaFpKoTQKS',
                     'created_at' => '2018-06-17 21:00:00',
